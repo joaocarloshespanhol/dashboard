2687928ffd1636e593bb8f883da1a070379a5e67
joaocarloshespanhol <joaocarlos.hespanhol@hotmail.com>
2023-10-24 22:33:33 -0300

    commit dashboard

---
+++ order.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Consultoria de Gestão Empresarial',
+        productNumber: '85029',
+        paymentStatus: '-',
+        status: 'Processando'
+    },
+    {
+        productName: 'Curso Figma Avançado',
+        productNumber: '97245',
+        paymentStatus: 'Não Pago',
+        status: 'Recusado'
+    },
+    {
+        productName: 'Mentoria sobre Vendas Online',
+        productNumber: '36452',
+        paymentStatus: 'Pago',
+        status: 'Autorizado'
+    },
+]
