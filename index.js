894e09cd4bbd6986cf77fe4d8688c01d019a8cd9
joaocarloshespanhol <joaocarlos.hespanhol@hotmail.com>
2023-10-27 14:15:42 -0300

    alteração de background, tamanho logo etc

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// const sideMenu = document.querySelector('aside');
-// const menuBtn = document.getElementById('menu-btn');
-// const closeBtn = document.getElementById('close-btn');
-
-// const darkMode = document.querySelector('.dark-mode');
-
-// menuBtn.addEventListener('click', () => {
-//     sideMenu.style.display = 'block';
-// });
-
-// closeBtn.addEventListener('click', () => {
-//     sideMenu.style.display = 'none';
-// });
-
-// darkMode.addEventListener('click', () => {
-//     document.body.classList.toggle('dark-mode-variables');
-//     darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
-//     darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
-// })
-
 
 Orders.forEach(order => {
     const tr = document.createElement('tr');
@@ -25,8 +5,8 @@ Orders.forEach(order => {
         <td>${order.productName}</td>
         <td>${order.productNumber}</td>
         <td>${order.paymentStatus}</td>
-        <td class="${order.status === 'Recusado' ? 'danger' : order.status === 'Processando' ? 'warning' : 'sucess'}">${order.status}</td>
-        <td class="primary">Details</td>
+        <td class="${order.status === 'Recusado' ? 'danger' : order.status === 'Processando' ? 'warning' : 'success'}">${order.status}</td>
+        <td class="primary">Detalhes</td>
     `;
     tr.innerHTML = trContent;
     document.querySelector('table tbody').appendChild(tr);
